49a21e3e688da51d282c760f92558b95407d8776
AlexVak <avakarchuk@me.com>
2019-03-13 17:21:10 +0200

    poll and vote repositories were added

---
+++ server/src/main/java/com/alexvak/polls/domain/ChoiceVoteCount.java
@@ -0,0 +1,16 @@
+package com.alexvak.polls.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChoiceVoteCount {
+    private Long choiceId;
+    private Long voteCount;
+
+    public ChoiceVoteCount(Long choiceId, Long voteCount) {
+        this.choiceId = choiceId;
+        this.voteCount = voteCount;
+    }
+}
